d87a3526a81aebd01d37b783ac217eb56b471599
tsours <timothytimtrinh@gmail.com>
2022-11-06 14:52:36 -0800

    Create function providing possible outcomes

--- RPS.js
+++ RPS.js
@@ -5,3 +5,21 @@ function getComputerChoice () {
 }
 
 console.log(getComputerChoice())
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
+        return 'You Win! Rock beats Scissors'
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper'){
+        return 'You Lose! Paper beats Rock'
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'You Lose! Rock beats Scissors'
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return 'You win! Scissors beats Paper'
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
+        return 'You win! Paper beats Rock'
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors'){
+        return 'You lose! Scissors beats Paper'
+    } else {
+        return `It's a tie!`
+    }
+} 
